5904725efe8efb96bb9764182f001cbc21cf2ab0
Cman3107 <cmacytes@gmail.com>
2026-01-17 20:21:49 +0300

    Quartz sync: Jan 17, 2026, 8:21 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "Cman3107.github.io/DIGITAL-BRAIN",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
